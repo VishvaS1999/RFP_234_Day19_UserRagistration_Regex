0072b069bbb857657b959834b7a212acf8aab9de
VishvaS1999 <gvishva2601@gmail.com>
2023-01-06 15:33:32 +0530

    Welcome User Ragistration

---
+++ src/com/regexuserregistration/WelcomeMessage.java
@@ -0,0 +1,8 @@
+package com.regexuserregistration;
+
+public class WelcomeMessage
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome Regex User Registration Program");
+    }
+}
