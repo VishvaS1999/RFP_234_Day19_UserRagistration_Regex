4d518ad0fb4aec9871fa59e17b1af197c5ab916b
VishvaS1999 <gvishva2601@gmail.com>
2023-01-06 16:00:28 +0530

    Check User Mobile Number Valid Or Not

--- src/com/regexuserregistration/UserRegistrationRegex.java
+++ src/com/regexuserregistration/UserRegistrationRegex.java
@@ -33,9 +33,20 @@ public class UserRegistrationRegex
         else
             System.out.println("Invalid Email");
     }
+    public static void mobile()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter Mobile Number With Country Code(space between code and number)");
+        String m = sc.nextLine();
+        if (Pattern.matches("^[1-9]{2}\s{1}[6-9]{1}[0-9]{9}$", m) == true)
+            System.out.println("Valid Mobile Number");
+        else
+            System.out.println("Invalid Mobile Number");
+    }
     public static void main(String[] args)
     {
         userFullName();
         email();
+        mobile();
     }
 }
