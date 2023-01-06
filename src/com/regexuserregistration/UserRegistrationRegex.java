f45c836cf79d1a6b6389fc61ae3cf7f199fc213f
VishvaS1999 <gvishva2601@gmail.com>
2023-01-06 15:37:47 +0530

    Check Last Name Valid Or Not

--- src/com/regexuserregistration/UserRegistrationRegex.java
+++ src/com/regexuserregistration/UserRegistrationRegex.java
@@ -17,10 +17,21 @@ public class UserRegistrationRegex
             System.out.println("Invalid First Name");
     }
 
+    public static void lastName()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter Last Name");
+        String lN = sc.nextLine();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lN) == true)
+            System.out.println("Valid Last Name");
+        else
+            System.out.println("Invalid Last Name");
+    }
+
 
     public static void main(String[] args)
     {
         firstName();
-
+        lastName();
     }
 }
